18685cf588368bdea9f53f4ac280f2f015b325b8
Doumi Athmane <ha_doumi@esi.dz>
2022-04-16 06:32:00 +0200

    Add logique controller/route

--- app/app.js
+++ app/app.js
@@ -13,15 +13,6 @@ client.connect();
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
-app.get('/utilisateur', (req, res) => {
-    client.query(`select * from "Utilisateur"`, (err, result) => {
-        if (!err) {
-            res.send(result.rows);
-        } else {
-            console.log(err)
-        }
-    })
-    client.end
-});
+
 
 app.listen(PORT, () => console.log(`Server running in port : ${PORT}`));
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { testFunction } from "../controllers/users.js"
+import { loginPatient, loginMedecin, loginAdministrateur, loginProchePatient } from "../controllers/users.js"
 const router = express.Router();
 
-router.get('/', testFunction)
+router.get('/patient', loginPatient)
+router.get('/medecin', loginMedecin)
+router.get('/administrateur', loginAdministrateur)
+router.get('/prochePatient', loginProchePatient)
+
 
 export default router;
