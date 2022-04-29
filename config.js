0410c00d1fdda02ec6563ebbf21ef2c806a38263
Doumi Athmane <ha_doumi@esi.dz>
2022-04-29 17:38:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import pg from "pg";
 const client = new pg.Client({
     host: "localhost",
     user: "postgres",
-    port: 5433,
+    port: 5432,
     password: "09062014bemA",
     database: "MyHealth"
 })
